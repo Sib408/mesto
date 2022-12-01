89bd2eca93b281b64e7e06d380d311130eb1a0ae
Irina Stepanets <sib408@yandex.ru>
2022-12-02 01:11:38 +0300

    update files

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,32 @@
-const editButton = document.querySelector('.button_edit');
-const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__button_close');
+const editButton = document.querySelector(".button_edit");
+const popup = document.querySelector(".popup");
+const popupClose = document.querySelector(".popup__button_close");
 
-
-editButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+editButton.addEventListener("click", openPopup);
+popupClose.addEventListener("click", closePopup);
 
 function openPopup() {
-  popup.classList.add('popup_opened');
-  nameInput.value = usernameProfile.textContent
-  descriptionInput.value = descriptionProfile.textContent
-
+  popup.classList.add("popup_opened");
+  nameInput.value = usernameProfile.textContent;
+  descriptionInput.value = descriptionProfile.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
 
-let formElement = document.querySelector('.popup__form');
+let formElement = document.querySelector(".popup__form");
 
-let nameInput = document.querySelector('.popup__input_type_username');
-let descriptionInput = document.querySelector('.popup__input_type_decsription');
-let usernameProfile = document.querySelector('.profile__username');
-let descriptionProfile = document.querySelector('.profile__description');
+let nameInput = document.querySelector(".popup__input_type_username");
+let descriptionInput = document.querySelector(".popup__input_type_decsription");
+let usernameProfile = document.querySelector(".profile__username");
+let descriptionProfile = document.querySelector(".profile__description");
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    usernameProfile.textContent = nameInput.value
-    descriptionProfile.textContent = descriptionInput.value
-    closePopup();
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  usernameProfile.textContent = nameInput.value;
+  descriptionProfile.textContent = descriptionInput.value;
+  closePopup();
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
-
+formElement.addEventListener("submit", handleFormSubmit);
